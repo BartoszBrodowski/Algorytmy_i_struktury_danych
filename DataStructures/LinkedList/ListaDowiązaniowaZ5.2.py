f0f8f9f84d9ac75cbbdad9442e3ad88bc6d28ab4
Bartosz <bar.brodowski@gmail.com>
2022-04-11 16:02:40 +0200

    Done, everything works fine

--- DataStructures/LinkedList/ListaDowiązaniowaZ5.2.py
+++ DataStructures/LinkedList/ListaDowiązaniowaZ5.2.py
@@ -1,26 +1,21 @@
 import random
 
+
 class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
         self.prev = None
 
+
 class LinkedList:
     def __init__(self):
-        
+
         # Robimy node, aby działały metody next i prev
         self.sentinel = Node(None)
         self.sentinel.next = self.sentinel
         self.sentinel.prev = self.sentinel
 
-    # def first_node(self):
-    #     # Jeśli next element wartownika to on sam, lista jest pusta
-    #     if self.sentinel.next == self.sentinel:
-    #         return None
-    #     else:
-    #         return self.sentinel.next
-    
     def wstaw(self, s):
         new_element = Node(s)
         if self.sentinel.next == self.sentinel:
@@ -44,19 +39,32 @@ class LinkedList:
         x = self.sentinel.next
         while x != self.sentinel:
             if x.value == s:
-                print("\n" + "Szukany element został znaleziony: " + str(x.value))
+                print("\n" + "Szukany element zostal znaleziony: " + str(x.value))
                 return x
             else:
                 x = x.next
+        print('Nie znaleziono elementu')
+        return None
 
     def usuń(self, s):
+        # Usuwa jeden węzeł z danym słowem
+        # x = self.sentinel.next
+        # while x != self.sentinel:
+        #     if x.value == s:
+        #         x.prev.next = x.next
+        #         x.next.prev = x.prev
+        #         x = None
+        #         return x
+        #     else:
+        #         x = x.next
+
+        # Usuwa wszystkie węzły z danym słowem
         x = self.sentinel.next
         while x != self.sentinel:
             if x.value == s:
                 x.prev.next = x.next
                 x.next.prev = x.prev
-                x = None
-                return x
+                x.value = None
             else:
                 x = x.next
 
@@ -81,33 +89,61 @@ class LinkedList:
         return nowa_lista
 
     def scal(self, l2):
-        temp = self.sentinel.prev
+        temp = self.sentinel.prev.next
+        temp2 = self.sentinel.prev
         self.sentinel.prev.next = l2.sentinel.next
-        l2.sentinel.prev.next = temp
-        self.drukuj()
-        return self
+        self.sentinel.prev = l2.sentinel.prev
+        l2.sentinel.prev.next = self.sentinel
+        l2.sentinel.prev = temp2
+
+        x = self.sentinel.next
+        while x != self.sentinel:
+            print(x.value, end=" ")
+            x = x.next
+
+
+print("---ZADANIE 1. a) Wstaw---")
 
 l = LinkedList()
-l.wstaw('What')
-l.wstaw('WW')
-l.wstaw('WW')
-l.wstaw('LOL')
-l.wstaw('K')
-l.wstaw('K')
+l.wstaw('TELEPORT')
+l.wstaw('LOGARYTM')
+l.wstaw('LOGARYTM')
+l.wstaw('WALKA')
+l.wstaw('KORONA')
+l.wstaw('KORONA')
+print("\n")
+
+print("---ZADANIE 1. b) Drukuj---")
+print("\n")
 l.drukuj()
+
+print("\n")
+print("---ZADANIE 1. c) Szukaj---")
+
+l.szukaj('TELEPORT')
+l.szukaj('WARCABY')
+
+# Wykomentowane, ponieważ zadanie 2 pokazuje wtedy wynik na wybrakowanej liście,
+# bo tworzy kopię listy dowiązaniowej l
+
+print("\n")
+print("---ZADANIE 1. d) Usun---")
+l.usuń('KORONA')
+l.drukuj()
+print("\n")
+
+print("\n")
+print("---ZADANIE 2. Bezpowtorzen---")
 print("\n")
 l.bezpowtorzeń()
 
+print("\n")
+print("---ZADANIE 3. Scal---")
+print("\n")
+
 lista_slow = ["drzewo", "jablko", "gruszka", "orzech"]
-l1 = LinkedList()
 l2 = LinkedList()
 for i in range(4):
-    l1.wstaw(lista_slow[i%4])
-    l2.wstaw(lista_slow[(i+2)%4])
-print('\n')
-l1.drukuj()
-print('\n')
-l2.drukuj()
-print('\n')
+    l2.wstaw(lista_slow[i])
 
 l.scal(l2)
